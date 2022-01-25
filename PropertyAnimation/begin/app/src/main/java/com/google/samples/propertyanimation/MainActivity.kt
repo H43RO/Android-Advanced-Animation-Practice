4fd098c120af12ba1e2827fb07b1d3d05b610819
H43RO <h2is1234@naver.com>
2022-01-25 22:36:07 +0900

    Scaling the Star

--- PropertyAnimation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ PropertyAnimation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -19,6 +19,7 @@ package com.google.samples.propertyanimation
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -95,6 +96,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(
+            star, scaleX, scaleY
+        )
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
     }
 
     private fun fader() {
