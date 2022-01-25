2161de90767951cd630eb10156d433f7ce22ac65
H43RO <h2is1234@naver.com>
2022-01-25 22:23:41 +0900

    Translating the Star

--- PropertyAnimation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ PropertyAnimation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -74,21 +74,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rotater() {
-        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+        val animator = ObjectAnimator.ofFloat(
+            star, View.ROTATION,
+            -360f, 0f
+        )
         animator.duration = 1000
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationStart(animation: Animator?) {
-                rotateButton.isEnabled = false
-            }
-            override fun onAnimationEnd(animation: Animator?) {
-                rotateButton.isEnabled = true
-            }
-        })
+        animator.disableViewDuringAnimation(rotateButton)
         animator.start()
-
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(
+            star, View.TRANSLATION_X,
+            200f
+        )
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
     }
 
     private fun scaler() {
@@ -101,6 +104,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun shower() {
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
     }
 
 }
